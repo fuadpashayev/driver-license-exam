131317e8a0e6ac8c4fdaa1f919f11b6edf4d2f9d
unknown <fuad.pashayev12@gmail.com>
2019-01-16 15:48:54 +0400

    change L1

--- database/seeds/CategoryTableSeeder.php
+++ database/seeds/CategoryTableSeeder.php
@@ -11,18 +11,18 @@ class CategoryTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Category::class, 5)->create()->each(function ($c) {
-            for($j = 1;$j <=5;$j++){
-                $parent =  $c->questions()->save(factory(App\Question::class)->make());
-                $parent->image_url = 'https://www.trafiktesten.dk/files/teoriproeve_billeder/teoriproeve_billeder_1404_image_3.jpg';
-                $parent->save();
-                for($i=1;$i<=3;$i++){
-                    $child = $c->questions()->save(factory(App\Question::class)->make());
-                    $child->parent_id = $parent->id;
-                    $child->answer = rand(0,1);
-                    $child->save();
-                }
-            }
-        });
+//        factory(App\Category::class, 5)->create()->each(function ($c) {
+//            for($j = 1;$j <=5;$j++){
+//                $parent =  $c->questions()->save(factory(App\Question::class)->make());
+//                $parent->image_url = 'https://www.trafiktesten.dk/files/teoriproeve_billeder/teoriproeve_billeder_1404_image_3.jpg';
+//                $parent->save();
+//                for($i=1;$i<=3;$i++){
+//                    $child = $c->questions()->save(factory(App\Question::class)->make());
+//                    $child->parent_id = $parent->id;
+//                    $child->answer = rand(0,1);
+//                    $child->save();
+//                }
+//            }
+//        });
     }
 }
