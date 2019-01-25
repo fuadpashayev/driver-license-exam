4b7c9ca3fc12359e194f83b37e42ddda21f9e2a6
unknown <fuad.pashayev12@gmail.com>
2019-01-25 13:07:06 +0400

    api changes

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -93,20 +93,52 @@ class QuestionController extends Controller
     }
 
 
-    public function categoryAll(){
-        $categories = Category::all();
-        $returnCategories = [];
-        foreach ($categories as $category){
-            $category->image_url = site_url().$category->image_url;
-            $returnCategories[] = $category;
+    public function categoryAll(Request $request){
+        $id = $request->id;
+        $order = $request->order;
+        if($id && $order){
+            $questions = Question::where('category_id',$id);
+            $questions = $questions->limit(25)->where("parent_id",null);
+            $questions = $questions->get();
+            if(count($questions)){
+                $status = "success";
+                $returnQuestions = [];
+                foreach ($questions as $index => $question){
+                    $question->image_url = site_url().$question->image_url;
+                    $question->audio_url = site_url().$question->audio_url;
+                    $sub_questions = Question::where("parent_id",$question->id)->get();
+                    $returnSubQuestions = [];
+                    foreach ($sub_questions as $sub_question){
+                        $sub_question->audio_url = site_url().$sub_question->audio_url;
+                        $returnSubQuestions[] = $sub_question;
+                    }
+                    $question->sub_questions = $returnSubQuestions;
+                    $returnQuestions[] = $question;
+                }
+
+                if($order!=null) $returnQuestions = $returnQuestions[$order-1];
+
+            }else{
+                $status = "error";
+                $questions = null;
+            }
+            return response()->json(['status'=>$status,'questions'=>$returnQuestions],200,["Accept"=>"application/json; charset=utf-8","Content-type"=>"application/json; charset=utf-8"],JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
+
+        }else {
+            $categories = Category::all();
+            $returnCategories = [];
+            foreach ($categories as $category) {
+                $category->image_url = site_url() . $category->image_url;
+                $returnCategories[] = $category;
+            }
+            if (count($returnCategories))
+                $status = "success";
+            else {
+                $status = "error";
+                $categories = null;
+            }
+            return response()->json(['status' => $status, 'categories' => $returnCategories], 200, ["Accept" => "application/json; charset=utf-8", "Content-type" => "application/json; charset=utf-8"], JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         }
-        if(count($returnCategories))
-            $status = "success";
-        else{
-            $status = "error";
-            $categories = null;
-        }
-        return response()->json(['status'=>$status,'categories'=>$returnCategories],200,["Accept"=>"application/json; charset=utf-8","Content-type"=>"application/json; charset=utf-8"],JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
     }
 
     public function questionsFromCategory(Request $request){
